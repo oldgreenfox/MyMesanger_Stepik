3a6c210a4d40f6786251c3f3519967e033b2db6b
oldgreenfox <oldgreenfox@gmail.com>
2022-07-22 23:07:14 +0300

    Serilization added

--- Message.cs
+++ Message.cs
@@ -1,5 +1,6 @@
 namespace MyMessanger_Stepik;
 
+[Serializable]
 public class Message
 {
     public string UserName { get; set; }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using MyMessanger_Stepik;
+using Newtonsoft.Json;
 
-Message msg = new Message();
-Console.WriteLine(msg.ToString());
+Message msg = new Message("GreenFox", "Hello", DateTime.UtcNow);
+string output = JsonConvert.SerializeObject(msg);
+Console.WriteLine(output);
+Message deserializeMsg = JsonConvert.DeserializeObject<Message>(output);
+Console.WriteLine(deserializeMsg.ToString());
+
+//{"UserName":"GreenFox","MessageText":"Hello","TimeStamp":"2022-07-22T20:03:57.784543Z"}
+//GreenFox <22.07.2022 20:03:57>: Hello
